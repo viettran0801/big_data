4983c035e3975f55ab62bda6a48d5692ac7a3a65
viettran0801 <viettran0801@gmail.com>
2022-12-10 20:01:53 +0700

    update

--- main.py
+++ main.py
@@ -1,38 +1,61 @@
 import requests
 import json
 import logging
+import csv
+import time
 
 
-def fetchData(code, year, logger):
-    res = requests.get(
-        f"https://diemthi.vnanet.vn/Home/SearchBySobaodanhFile?code={code}&nam={year}"
-    )
-    content = json.loads(res.text)
-    try:
-        if content["message"] == "success":
-            if len(content["result"]) > 0:
-                logger.info(
-                    f'{content["message"]} {content["result"][0]["Code"]} {content["result"][0]["Toan"]}'
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler(f"{int(time.time())}_error.txt")
+formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+
+year = 2019
+start_provice = 1
+start_index = 1  # tiep tuc tu so bao danh
+
+for p in range(start_provice, 65):
+    with open(f"{year}_{p}_data.csv", "a", 1) as csvfile:
+        writer = csv.writer(csvfile)
+        s = start_index if p == start_provice else 1
+
+        for i in range(s, 1_000_000):
+            code = f"{p:02}{i:06}"
+            try:
+                res = requests.get(
+                    f"https://diemthi.vnanet.vn/Home/SearchBySobaodanhFile?code={code}&nam={year}"
                 )
-            else:
-                logger.info(f'{content["message"]} EMPTY')
-        else:
-            logger.error(f"ERROR {code} {content}")
-    except:
-        logger.error(f"ERROR {code} {content}")
-
-
-def main():
-    logger = logging.getLogger("crawler")
-    logger.setLevel(logging.DEBUG)
-    file_handler = logging.FileHandler("logs.txt")
-    formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s %(message)s")
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
-
-    for i in range(1, 10000):
-        fetchData(f"0101{i:04}", 2019, logger)
-
-
-if __name__ == "__main__":
-    main()
+                content = json.loads(res.text)
+                if content["message"] == "success":
+                    if len(content["result"]) > 0:
+                        raw = content["result"][0]
+                        writer.writerow(
+                            [
+                                code,
+                                raw["Toan"],
+                                raw["NguVan"],
+                                raw["NgoaiNgu"],
+                                raw["VatLi"],
+                                raw["HoaHoc"],
+                                raw["SinhHoc"],
+                                raw["LichSu"],
+                                raw["DiaLi"],
+                                raw["GDCD"],
+                            ]
+                        )
+                    else:
+                        writer.writerow([code, "", "", "", "", "", "", "", "", ""])
+                        logger.warning(f"{year} {code} empty")
+                elif (
+                    content["message"]
+                    == "Số báo danh không đúng. Bạn vui lòng nhập lại."
+                ):
+                    logger.warning(f"{year} provice {p} end")
+                    break
+            except KeyboardInterrupt:
+                logger.info(f"stop")
+                exit(0)
+            except:
+                logger.error(f"{year} {code} unrecognize error")
